7adbf998a36d86a47f82f19a4b8d7562f0cb4aa5
Roaney Faria Adami <roaney91@hotmail.com>
2025-10-24 15:44:17 -0300

    update

---
+++ exercicios/ex02/variaveis02.py
@@ -0,0 +1,14 @@
+preco = 10
+print(preco)
+preco = float(preco)
+print(preco)
+print(preco / 2)
+
+price = 10.50
+print(price)
+price = int(price)
+print(price)
+
+price = 20
+print(price / 2)
+print(price // 2)
---
+++ exercicios/ex02/variaveis03.py
@@ -0,0 +1,7 @@
+age = 30
+price = 10.50
+print(age, price)
+texto = f"idade {age} preço {price}"
+print(f"idade {age} preço {price}")
+print(texto)
+
