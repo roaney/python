79384f30479c319179881d625c93830a6321b045
Roaney Faria Adami <roaney91@hotmail.com>
2025-10-24 11:06:39 -0300

    Create variaveis.py

---
+++ exercicios/ex02/variaveis.py
@@ -0,0 +1,9 @@
+AGE1 = 34
+name = "Roaney"
+print(f'Meu nome é {name}, tenho {AGE1} anos de idade.')
+age2 = 40
+print(f'Meu nome é {name}, tenho {age2} anos de idade.')
+age2 - AGE1
+print(age2 - AGE1)
+AGE1 = 1
+print(AGE1)
