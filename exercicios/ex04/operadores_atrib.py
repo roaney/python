dad56db721ff2961b1f93c82016c7e98f4c7a5d6
Roaney Faria Adami <roaney91@hotmail.com>
2025-10-25 22:08:55 -0300

    Create operadores_atrib.py

---
+++ exercicios/ex04/operadores_atrib.py
@@ -0,0 +1,14 @@
+saldo = 100
+print(saldo)
+saldo = saldo + 100
+print(saldo)
+saldo += 100
+print(saldo)
+saldo -= 100
+print(saldo)
+saldo *= 2
+print(saldo)
+saldo /= 2
+print(saldo)
+saldo //= 2
+print(saldo)
