48c9969abe2c0183af3cd0e623ec6d7d714e988e
Roaney Faria Adami <roaney91@hotmail.com>
2025-10-26 14:15:42 -0300

    Create operadores_log.py

---
+++ exercicios/ex04/operadores_log.py
@@ -0,0 +1,15 @@
+n1 = 10
+n2 = 15
+n3 = 20
+print(n1 > n2 and n2 > n3)
+print(n1 < n2 and n2 < n3)
+print(n1 < n2 and n2 > n3)
+print(n1 > n2 and n2 < n3)
+print(n1 > n2 or n2 > n3)
+print(n1 < n2 or n2 < n3)
+print(n1 < n2 or n2 > n3)
+print(n1 > n2 or n2 < n3)
+print(not(n1 > n2 and n2 > n3))
+print(not(n1 < n2 and n2 < n3))
+print(not(n1 < n2 and n2 > n3))
+print(not(n1 > n2 and n2 < n3))
