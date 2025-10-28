f9dbbf998b9f1889ad7c597b114a13a9adb28c77
Roaney Faria Adami <roaney91@hotmail.com>
2025-10-28 14:09:22 -0300

    Create for03.py

---
+++ exercicios/ex06/for03.py
@@ -0,0 +1,14 @@
+for num in range(4):
+    print(num, end = " ")
+print()
+
+for num in range(0, 4, 1):
+    print(num, end = " ")
+print()
+
+for num in range(1, 11, 1):
+    print(num, end = " ")
+print()
+
+for num in range(10, 0, -1):
+    print(num, end = " ")
