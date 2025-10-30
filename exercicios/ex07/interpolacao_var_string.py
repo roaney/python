92e83c11f697ed5471950438902779c75f3923f7
Roaney Faria Adami <roaney91@hotmail.com>
2025-10-30 17:05:53 -0300

    Update interpolacao_var_string.py

--- exercicios/ex07/interpolacao_var_string.py
+++ exercicios/ex07/interpolacao_var_string.py
@@ -2,4 +2,8 @@ nome = "Roaney"
 print(f"Meu nome é {nome}.")
 
 numero = 1.938408439
-print(f"O número é {numero:.3f}")
+print(f"O número é {numero:.2f}")
+
+print(nome, "Adami")
+print(nome + " Adami")
+print(f"{nome} Adami")
