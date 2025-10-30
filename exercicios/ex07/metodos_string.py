952cd7776e82d87f3cb641105b6186566d2d99d1
Roaney Faria Adami <roaney91@hotmail.com>
2025-10-30 11:17:26 -0300

    Create metodos_string.py

---
+++ exercicios/ex07/metodos_string.py
@@ -0,0 +1,15 @@
+curso = "pYtHon"
+print(curso.upper())
+print(curso.lower())
+print(curso.title())
+
+curso = "   Python   "
+print(curso.strip())
+print(curso.lstrip())
+print(curso.rstrip())
+
+curso = "Python"
+print(curso.center(10, "#"))
+
+lista = ["arroz", "café", "manga"]
+print(", ".join(lista))
