e631cc2f51904b9a263f648133c5a1a9590c50b3
Roaney Faria Adami <roaney91@hotmail.com>
2025-10-30 17:57:12 -0300

    Create fatiamento_string.py

---
+++ exercicios/ex07/fatiamento_string.py
@@ -0,0 +1,9 @@
+nome = "Roaney Faria Adami"
+nom = " 01234567891011121314151617"
+print(nome [0])
+print(nome [:6])
+print(nome [7:])
+print(nome [7:12])
+print(nome [0::2])
+print(nome [:])
+print(nome [::-1])
