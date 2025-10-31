b2287cc585d56f29fab4e52f00c36b00d266e38c
Roaney Faria Adami <roaney91@hotmail.com>
2025-10-31 11:13:41 -0300

    Update string_multiplas.py

--- exercicios/ex07/string_multiplas.py
+++ exercicios/ex07/string_multiplas.py
@@ -2,6 +2,15 @@ texto = """Casa amarela, céu azul
 rua cinza, água transparente,
 nova vertente."""
 print(texto)
-print("""Casa amarela, céu azul
+
+print("""
+Casa amarela, céu azul
 rua cinza, água transparente,
-nova vertente.""")
+nova vertente.
+""")
+
+print("""
+Casa amarela, céu azul
+        rua cinza, água transparente,
+    nova vertente.
+""")
