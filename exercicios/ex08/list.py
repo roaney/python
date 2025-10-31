2a4c58fb7cbc6c898f096ea3cedd97947386c92e
Roaney Faria Adami <roaney91@hotmail.com>
2025-10-31 18:15:39 -0300

    update

---
+++ exercicios/ex08/list.py
@@ -0,0 +1,15 @@
+frutas = ["maçã", "laranja", "uva"]
+first_item = frutas[0]
+print(first_item)
+print(first_item[0])
+print(frutas[0])
+print(frutas[-1])
+
+letras = list("python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["ferrari", 2025, True]
+print(carro)
---
+++ exercicios/ex08/list_matriz.py
@@ -0,0 +1,8 @@
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6, 10],
+    [7, 8, 9]
+]
+print(matriz[1][1])
+print(matriz[2][1])
+print(matriz[1][3])
